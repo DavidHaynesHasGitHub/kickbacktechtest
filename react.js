8e3fcae4d5bb6c21870efbc0e1c300d7c1e13468
David Haynes <DavidHaynesVictoria@gmail.com>
2018-04-11 10:05:38 -0700

    untested yet but react component completed.

--- react.js
+++ react.js
@@ -18,6 +18,10 @@ class App extends React.Component{
     componentDidMount(){
 
     }
+
+    click(){
+      alert('This form doesnt actually deposit funds but it would have if it was a real form')
+    }
     action(amount){
         this.setState({depositAmount: amount})
     }
@@ -28,6 +32,7 @@ class App extends React.Component{
             if(this.state.depositAmount === amount){
                 classNames = 'deposit-button-selected'
             }
+
             return(
             <div className={classNames}
                 onClick={() => this.action(amount)}>
@@ -36,27 +41,41 @@ class App extends React.Component{
             )
         })
     }
-    creditCardNumber(event){
+    newCreditCardNumber(event){
         this.setState({creditCardNumber: event.target.value})
     }
-     creditCardName(event){
+     newCreditCardName(event){
             this.setState({creditCardName: event.target.value})
         }
-     creditCardExpiration(event){
+     newCreditCardExpiry(event){
             this.setState({creditCardExpiration:    event.target.value})
         }
-     creditCardCVC(event){
+     newCreditCardCVC(event){
             this.setState({creditCardCVC:    event.target.value})
         }
-  render(){
-      <div className="main">
+    render(){
+        <div className="main">
           <div className="inner">
             <h1 className='header'>Deposit Funds</h1>
             <div className='subhead'>$123,456 won in the last 24 hours</div>
             <div className="creditCard">
-                <div className="amounts">
-                    {this.renderDeposits()}
-                </div>
-
+              <div className="amounts">
+                  {this.renderDeposits()}
+              </div>
+              <div className="creditCardNumberContainer">
+                <input className="creditCardNumberInput" type="text" placeholder-"Credit Card Number"
+                value={this.state.creditCardNumber}
+                onChange={this.newCreditCardNumber}/>
+              </div>
+              <div className="creditCardInformation">
+                <input className="nameOnCard" type="text" placeholder="Cardholders Name" value={this.state.creditCardName} onChange={this.newCreditCardName}/>
+                <input className="expiryOnCard" type="text" placeholder="Card Expiry MM/YY" value={this.state.creditCardExpiry} onChange={this.newCreditCardExpiry}/>
+                <input className="cardCVC" type="text" placeholder="CVC" value={this.state.creditCardCVC} onChange={this.newCreditCardCVC}/>
+              </div>
+              <button className="depositFunds" onClick={this.click}>Deposit Funds</button>
+              </div>
+            </div>
+          </div>
   }
 }
+React.render(<app />, document.getElementById("app"));
